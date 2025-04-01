1ed9e24a41386fb8f97dc0c2ca32ece63c813364
michalziolek78 <michal.ziolek78@gmail.com>
2025-04-01 11:35:36 +0200

    Add files via upload

---
+++ szablon2.php
@@ -0,0 +1,53 @@
+
+
+
+
+<?php
+require('top.inc');
+?>
+
+<div id="calosc">
+
+<div id="tresc"><!-- srodek -->
+
+<div id="lewa">
+
+<div id="lewa2">
+<p class="tresc">
+	Witaj w muzycznej krainie Dwie Nuty!<br /><br />
+	Pragnê tutaj zaprezentowaæ projekty muzyczne, które s± odzwierciedleniem
+	moich pasji, czylli komponowania nowoczesnej muzyki elektronicznej oraz moich muzycznych zainteresowañ.
+	Dwa projekty, dwa ró¿ne spojrzenia na muzykê.<br />
+	Projekt Extreme Discotheque to muzyka z gatunku Psychedelic Trance. 
+	Utwory jakie znajdziesz na tutejszych stronach prezentuj± ró¿ne odmiany 
+	tego gatunku.
+	Projekt Secret Energy to muzyka Electro, Techno, Tech-House i Progressive.
+</p>
+</div>
+
+<div id="dolmirror"></div>
+
+</div><!-- koniec lewa -->
+
+<div id="prawa">
+
+<div id="prawa2">
+<p class="tresc2">
+	<h1>Szablon</h1>
+</p>
+</div>
+
+<div id="dolmirror2"></div>
+
+</div><!-- koniec prawa -->
+
+</div> <!--koniec tresc i srodka-->
+
+</div>
+
+<?php
+require('stopka.inc');
+?>
+
+
+
